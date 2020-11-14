dd41ae938a3b6ebcd3daebe69760ab8f1bea4fd6
Wipu Kumthong <wipu25@gmail.com>
2020-11-14 08:38:49 +0700

    Added file2.py

---
+++ file2.py
@@ -0,0 +1,13 @@
+def main():
+    var l = {'a','b','c','d','e'}
+    xy=0
+    for i in range(len(l)):
+        x = l[i] y = l[i+1]
+    try:
+        x = next(i for i, n in enumerate(l) if n > 0)
+    except StopIteration:
+        print('No positive numbers')
+    else:
+        print('The index of the first positive number is', x)
+    if xy==True
+	return x
