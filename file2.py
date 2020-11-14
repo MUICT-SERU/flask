9b29639d377e503384254487e7edb941b17b44be
Wipu Kumthong <wipu25@gmail.com>
2020-11-14 08:19:08 +0700

    Delete file

--- file2.py
+++
@@ -1,8 +0,0 @@
-def main():
-	ls1 = [1, 2, 3, 4, 5]
-	ls2 = [4, 5, 6, 7, 8]
-	elements_in_both = []
-	for element in ls1:
-  		if element in ls2:
-   		elements_in_both.append(element)
-	print(elements_in_both)
